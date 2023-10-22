7bc430c76ecab3721861b971270f2355246cf873
Spy <Spyngunga@gmail.com>
2023-10-22 04:49:25 +0300

    Completed AssignmenT

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1;
+let num2;
+num1 = 31;
+num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random()*100);
+
+let num3;
+let num4;
+num3 = 44;
+num4 = 10;
+
+let mod = num3 % num4;
+
+let set =[4, 9, 12, 20];
+
+let max = Math.max(20);
+
